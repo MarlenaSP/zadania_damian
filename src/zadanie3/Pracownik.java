9c375df8f8f5bcb1535ddafebe0c1d20ccd48d8d
Marlena <sosnowska.marlena@wp.pl>
2023-07-19 20:56:36 +0200

    Zadanie3 poprawiona klasa Pracownik

--- src/zadanie3/Pracownik.java
+++ src/zadanie3/Pracownik.java
@@ -1,5 +1,6 @@
 package zadanie3;
 
+
 public class Pracownik {
     private String imie;
     private String nazwisko;
@@ -8,26 +9,27 @@ public class Pracownik {
     private int dzial;
 
     public Pracownik(String imie, String nazwisko, double placa, char plec, int dzial) {
-        this.imie = this.imie;
-        this.nazwisko = this.nazwisko;
-        this.placa = this.placa;
-        this.plec = this.plec;
-        this.dzial = this.dzial;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.placa = placa;
+        this.plec = plec;
+        this.dzial = dzial;
     }
 
-    public boolean czyPracujeWDziale (int dzial){
+    public boolean czyPracujeWDziale(int dzial) {
         return this.dzial == dzial;
     }
 
     @Override
     public String toString() {
-        return "Pracownik{" +
-                "imie='" + imie + '\'' +
-                ", nazwisko='" + nazwisko + '\'' +
-                ", placa=" + placa +
-                ", plec=" + plec +
-                ", dzial=" + dzial +
-                '}';
+        return "imie= " + imie +
+                ", nazwisko= " + nazwisko +
+                ", placa= " + placa +
+                ", plec= " + plec +
+                ", dzial= " + dzial;
     }
 
+    public double getPlaca() {
+        return placa;
+    }
 }
